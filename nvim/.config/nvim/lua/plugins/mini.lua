57e272f04b26efeca0ea1facb818bd883856be51
Prashant Deva <prashant@astradot.com>
2025-01-05 22:39:36 +0000

    format lua

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,3 +1,8 @@
-return {
-    { 'echasnovski/mini.nvim' , config = function() require('mini.statusline').setup({ use_icons = true }) end }
-}
+return {{
+    'echasnovski/mini.nvim',
+    config = function()
+        require('mini.statusline').setup({
+            use_icons = true
+        })
+    end
+}}

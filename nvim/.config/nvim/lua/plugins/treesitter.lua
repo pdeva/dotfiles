a99e4b43a82ebbcdf1bede1b9211c56ca097dca8
Prashant Deva <prashant@astradot.com>
2025-01-06 07:09:38 +0000

    more ts grammers

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,8 +3,9 @@ return {{
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup {
-            ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "markdown", "rust", "make", "toml", "json",
-                                "yaml", "html", "go", "java", "starlark", "python", "gomod", "gosum"},
+            ensure_installed = {"c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "rust", "make", "toml", "json",
+                                "yaml", "html", "go", "java", "starlark", "python", "gomod", "gosum", "hcl",
+                                "terraform", "proto", "zig"},
             auto_install = false,
             highlight = {
                 enable = true,

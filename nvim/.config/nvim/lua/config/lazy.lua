95f6581a9045f03c6fed2442ff04c3c1ba1bb0c1
Prashant Deva <prashant@astradot.com>
2025-01-07 18:30:50 +0000

    check updates

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -22,10 +22,12 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
     spec = {{
         import = "plugins"
-    }}
+    }},
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
     --   install = { colorscheme = { "habamax" } },
     -- automatically check for plugin updates
-    --   checker = { enabled = true },
+    checker = {
+        enabled = true
+    }
 })
